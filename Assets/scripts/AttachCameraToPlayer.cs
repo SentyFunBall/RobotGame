dddfbe3914755db33ddc10d835ce47c6eac94055
SentyFunball <sentyfb@gmail.com>
2026-09-12 17:13:24 -0700

    Make player camera motion smooth, add custom skybox with orange/smoky colors

--- Assets/scripts/AttachCameraToPlayer.cs
+++ Assets/scripts/AttachCameraToPlayer.cs
@@ -5,22 +5,35 @@ public class AttachCameraToPlayer : MonoBehaviour
     public GameObject playerObj;
 
     // Default offsets
-    public Vector3 posOff = new Vector3(3.65f, 7.88f, -1.09f);
-    public Vector3 rotOff = new Vector3(63.4f, -52.1f, -2.037f);
+    [SerializeField]
+    [Tooltip("Position offset of the camera relative to the player")]
+    private Vector3 posOff = new Vector3(3.65f, 7.88f, -1.09f);
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    [Tooltip("Rotation offset of the camera relative to the player")]
+    private Vector3 rotOff = new Vector3(63.4f, -52.1f, -2.037f);
+
+    [SerializeField]
+    [Tooltip("Smooth factor for camera movement and rotation")]
+    private float smoothFactor = 0.125f;
+
+    // LateUpdate is called once per frame, after all Update functions have been called
+    void FixedUpdate()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        // Just in case the player object is not assigned, do nothing
         if (playerObj != null)
         {
-            transform.position = playerObj.transform.position + posOff;
-            transform.rotation = playerObj.transform.rotation * Quaternion.Euler(rotOff);
+            // Smoothly move and rotate the camera to follow the player with the specified offsets and smooth factor
+            transform.position = Vector3.Lerp(
+                transform.position,
+                playerObj.transform.position + posOff,
+                smoothFactor
+            );
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                playerObj.transform.rotation * Quaternion.Euler(rotOff),
+                smoothFactor
+            );
         }
     }
 }
